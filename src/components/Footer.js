9b7e8c9f83d76ea89c286817219d9d691472c193
Michalis Mavromatakis <84912771+michalismav19@users.noreply.github.com>
2024-04-01 14:51:53 +0300

    Change year of site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import './Footer.scss';
+import React from "react";
+import "./Footer.scss";
 // import Dots from 'react-carousel-dots';
 
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
- 
-import MagicSliderDots from 'react-magic-slider-dots';
-import 'react-magic-slider-dots/dist/magic-dots.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import MagicSliderDots from "react-magic-slider-dots";
+import "react-magic-slider-dots/dist/magic-dots.css";
 
 function Footer() {
-
-    return (
-        <div className="footer">
-            <div className="footer-container">
-                <div className="footer-text-section">
-                    {/* <span className="footer-text">Designed and Developed by John Doe</span> */}
-                    <span className="footer-text">&copy; 2022 Michalis Mavromatakis</span>
-                </div>
-            </div>
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="footer-text-section">
+          {/* <span className="footer-text">Designed and Developed by John Doe</span> */}
+          <span className="footer-text">&copy; 2024 Michalis Mavromatakis</span>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
