dfae23d33b9ea1718fcef5afdc73083ecf038e5e
Michalis Mavromatakis <84912771+michalismav19@users.noreply.github.com>
2024-04-01 14:24:02 +0300

    Update pdf resume and profile photo

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,46 +1,47 @@
-import React from 'react';
-import './Aboutme.scss';
-import prof_pic from '../images/michalis-profile.jpeg';
-import Typist from 'react-typist'; //npm install react-typist --save
-import SocialMedia from './SocialMedia';
+import React from "react";
+import "./Aboutme.scss";
+import prof_pic from "../images/michalis-profile-photo.png";
+import Typist from "react-typist"; //npm install react-typist --save
+import SocialMedia from "./SocialMedia";
 
 function Aboutme() {
-
-    const setDisplay = () => {
-        if(window.innerWidth <= 1025) {
-            return "block";
-        } else {
-            return "none";
-        } 
+  const setDisplay = () => {
+    if (window.innerWidth <= 1025) {
+      return "block";
+    } else {
+      return "none";
     }
-    
-    return (
-        <div className="aboutme">
-            <div className="aboutme-container">
-                <img src={prof_pic} alt="Profile Picture"></img>
-                <div className="aboutme-main">
-                    <Typist>
-                        <span className="profile-surname">Michalis Mavromatakis</span>
-                        <br />
-                        <span className="profile-title">Lead Full Stack Developer</span>
-                    </Typist>
-                    {/* <span className="profile-info">
+  };
+
+  return (
+    <div className="aboutme">
+      <div className="aboutme-container">
+        <img src={prof_pic} alt="Profile Picture"></img>
+        <div className="aboutme-main">
+          <Typist>
+            <span className="profile-surname">Michalis Mavromatakis</span>
+            <br />
+            <span className="profile-title">
+              Lead-Senior Full Stack Developer
+            </span>
+          </Typist>
+          {/* <span className="profile-info">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                         when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                     </span> */}
-                    
-                </div>
-                <span className="download-resume-btn">
-                    <a href="/files/Michalis-Mavromatakis-resume.pdf" download>Download Resume</a>
-                </span>
-            </div>
-
-            {/* Social Media Section */}
-            <SocialMedia className="mobile"/>
-
         </div>
-    )
+        <span className="download-resume-btn">
+          <a href="/files/MichalisMavromatakisResume.pdf" download>
+            Download Resume
+          </a>
+        </span>
+      </div>
+
+      {/* Social Media Section */}
+      <SocialMedia className="mobile" />
+    </div>
+  );
 }
 
-export default Aboutme
+export default Aboutme;
